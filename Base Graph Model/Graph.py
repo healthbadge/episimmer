cb1ce02b5d10118f51912c6501b7909023b25619
Inavamsi Enaganti <inav65@gmail.com>
2020-09-08 23:26:37 +0530

    Add files via upload

--- Base Graph Model/Graph.py
+++ Base Graph Model/Graph.py
@@ -70,3 +70,58 @@ class StratifiedGraph():
 		for i in range(n):
 			dsum+=len(self.adj_list[i])
 		self.average_degree=2*dsum/n
+
+class FamilyGraph():
+	def __init__(self,n,p,family_sizes,connected=True):
+		#family_sizes is cumulative percentage 
+		#30% size 1 families and 70% size 4 => [0.3,0.3,0.3,1]
+		self.n=n
+		self.p=p
+		self.connected=connected
+
+		self.adj_list=[]
+		for i in range(n):
+			self.adj_list.append([])
+
+		for i in range(n):
+			for j in range(i+1,n):
+				if random.random()<p:
+					self.adj_list[i].append(j)
+					self.adj_list[j].append(i)
+
+		r=random.random()
+		f_size=-1
+		for fs,p in enumerate(family_sizes):
+			if r<p:
+				f_size=fs+1
+				break
+		index_so_far=0
+		while index_so_far+f_size<=n:
+			family_list=range(index_so_far,index_so_far+f_size)
+			index_so_far+=f_size
+			self.connect_family(family_list)
+		if index_so_far<n-1:
+			self.connect_family(range(index_so_far,n))
+
+		if self.connected:
+			for i in range(n):
+				if self.adj_list[i]==[]:
+					allowed_values = list(range(0, n))
+					allowed_values.remove(i)
+					j = random.choice(allowed_values)
+					self.adj_list[i].append(j)
+					self.adj_list[j].append(i)
+
+		for i in range(n):
+			self.adj_list[i]=list(set(self.adj_list[i]))
+
+		dsum=0
+		for i in range(n):
+			dsum+=len(self.adj_list[i])
+		self.average_degree=2*dsum/n
+
+	def connect_family(self,family_list):
+		for i in range(len(family_list)-1):
+			for j in range(i+1,len(family_list)):
+				self.adj_list[family_list[i]].append(family_list[j])
+				self.adj_list[family_list[j]].append(family_list[i])
