f1f0b4a4c421c85fcd723163c56de949353440a9
Inavamsi Enaganti <inav65@gmail.com>
2020-09-10 00:37:21 +0530

    Delete Generate.py

--- Generate Graph/Generate.py
+++
@@ -1,8 +0,0 @@
-import Graph
-import sys
-
-n=int(sys.argv[1])
-p=float(sys.argv[2])
-graph_obj = Graph.RandomGraph(n,p,False)
-graph_obj.write_to_simple_adjacency_list_file()
-graph_obj.write_to_simple_edge_list_file()
