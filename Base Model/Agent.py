e083ee4c7dc86df2c3f14ac391dc0386172b8cef
Inavamsi Enaganti <inav65@gmail.com>
2020-09-27 17:06:16 +0530

    Add files via upload

--- Base Model/Agent.py
+++ Base Model/Agent.py
@@ -1,14 +1,43 @@
 import random
 import copy
-import matplotlib.pyplot as plt
-from matplotlib import colors
 import numpy as np
 
 class Agent():
-	def __init__(self,state,index):
+	def __init__(self,state,info_dict):
 		self.state=state
-		self.index=index
-		self.neighbours=[]
 		self.next_state=None
-		self.quarantined=False
-		self.HLA_type=None
+		self.contact_list=[]
+		self.location_list=[]
+		self.info=info_dict
+		self.index=info_dict['Agent Index']
+		self.event_probabilities=[]
+		
+		self.schedule_time_left=None
+		self.testing_history=[] #tuple of (<testing time(Time tested)>, <testing type(Pool, Antigen,...)>, <test machine id(unique machine)>, <result(Positive, Negative, Viral Load...)>)
+		self.compliance = None #Can be a dictionary of Guideline compliance, Restriction compliancce, Governemnt Policy compliance
+
+	def add_contact(self,contact_dict):
+		self.contact_list.append(contact_dict)
+
+	def add_event_result(self,p):
+		self.event_probabilities.append(p)
+
+	def new_day(self):
+		self.next_state=None
+		self.contact_list=[]
+		if self.schedule_time_left!=None:
+			self.schedule_time_left-=1
+			if self.schedule_time_left <=0:
+				self.schedule_time_left=None
+
+	def update_state(self):
+
+		if self.next_state==None:
+			return
+		self.state=self.next_state
+		self.next_state=None
+
+	def set_next_state(self,state_info):
+		next_state,schedule_time=state_info
+		self.next_state=next_state
+		self.schedule_time_left=schedule_time
