366b4fb04f82e31ab8f332c363b93530375c910d
Inavamsi Enaganti <inav65@gmail.com>
2020-09-30 17:44:23 +0530

    Updated according to changes in model

--- Base Model/Examples/Example_3/Generate_model.py
+++ Base Model/Examples/Example_3/Generate_model.py
@@ -9,25 +9,35 @@ def generate_model():
 
 	#This function is user defined, based on the parameters the user has inputed in agents file and interaction/contact file
 	#This function represents the probability of getting infected during a single interaction/contact
-	def probabilityOfInfection_fn(p_inf_symp,p_inf_asymp,contact_agent,c_dict):
+	def probabilityOfInfection_fn(p_infected_states_list,contact_agent,c_dict):
+		
+		p_inf_symp,p_inf_asymp=p_infected_states_list[0],p_infected_states_list[1]
+		#EXAMPLE 1
+		if contact_agent.state=='Symptomatic':
+			return math.tanh(float(c_dict['Time Interval']))*p_inf_symp
+		elif contact_agent.state=='Asymptomatic':
+			return math.tanh(float(c_dict['Time Interval']))*p_inf_asymp
+		else:
+			return 0
 
 		#Example 2
 		if contact_agent.state=='Symptomatic':
-			return (float(c_dict['Room Ventilation'])+0.1)*p_inf_symp
+			return math.tanh(float(c_dict['Time Interval'])*float(c_dict['Intensity']))*p_inf_symp
 		elif contact_agent.state=='Asymptomatic':
-			return (float(c_dict['Room Ventilation'])+0.1)*p_inf_asymp
+			return math.tanh(float(c_dict['Time Interval'])*float(c_dict['Intensity']))*p_inf_asymp
 		else:
 			return 0
 
 
 	model=Model.ScheduledModel()
-	model.insert_state('Susceptible',None, None,model.p_infection(0.3,0.1,probabilityOfInfection_fn,{'Exposed':1}),False)
-	model.insert_state('Exposed',5,2,model.scheduled({'Symptomatic':0.7,'Asymptomatic':0.3}),False)
-	model.insert_state('Symptomatic',7,1,model.scheduled({'Recovered':0.7, 'ICU':0.3}),True)
-	model.insert_state('ICU',10,5,model.scheduled({'Recovered':0.1,'Dead':0.9}),False)
+	model.insert_state('Susceptible',None, None,model.p_infection([0.3,0.1],probabilityOfInfection_fn,{'Exposed':1}),False)
+	model.insert_state('Exposed',5,2,model.scheduled({'Symptomatic':0.3,'Asymptomatic':0.7}),False)
+	model.insert_state('Symptomatic',11,5,model.scheduled({'Recovered':1}),True)
 	model.insert_state('Asymptomatic',6,3,model.scheduled({'Recovered':1}),True)
-	model.insert_state('Recovered',None, None,model.scheduled({'Recovered':1}),False)
-	model.insert_state('Dead',None, None,model.scheduled({'Dead':1}),False)
+	model.insert_state('Recovered',100, 0,model.scheduled({'Recovered':1}),False)
+
+	model.set_event_contribution_fn(None)
+	model.set_event_recieve_fn(None)
 
 	return model
 
