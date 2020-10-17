ee106fb1e413b9bbbe4b0f8832424f02e94a3cda
Inavamsi Enaganti <inav65@gmail.com>
2020-10-17 18:00:31 +0530

    Changed rates to make it a simpler example

--- examples/Example_SIR/Generate_model.py
+++ examples/Example_SIR/Generate_model.py
@@ -7,12 +7,12 @@ import math
 def generate_model():
 
 	#Example Stochastic Model
-	'''
+	
 	individual_types=['Susceptible','Infected','Recovered','ICU','Dead']
 	infected_states=['Infected']
 	model=Model.StochasticModel(individual_types,infected_states)
 	model.set_transition('Susceptible', 'Infected', model.p_infection(None,None))
-	model.set_transition('Infected', 'Recovered', model.p_standard(0.1))
+	model.set_transition('Infected', 'Recovered', model.p_standard(0.2))
 	model.set_transition('Infected', 'ICU', model.p_standard(0.1))
 	model.set_transition('ICU', 'Recovered', model.p_standard(0.2))
 	model.set_transition('ICU', 'Dead', model.p_standard(0.05))
@@ -20,10 +20,10 @@ def generate_model():
 	#Example Scheduled Model
 	model=Model.ScheduledModel()
 	model.insert_state('Susceptible',None, None,model.p_infection(None,None,{'Infected':1}),True)
-	model.insert_state('Infected',7,1,model.scheduled({'Recovered':0.7, 'ICU':0.3}),True)
+	model.insert_state('Infected',3,1,model.scheduled({'Recovered':0.7, 'ICU':0.3}),True)
 	model.insert_state('Recovered',None, None,model.scheduled({'Recovered':1}),False)
 	model.insert_state('ICU',4,1,model.scheduled({'Recovered':1}),False)
-	
+	'''
 
 
 	def event_contribute_fn(agent,event_info,location,current_time_step):
@@ -33,7 +33,7 @@ def generate_model():
 
 	def event_recieve_fn(agent,ambient_infection,event_info,location,current_time_step):
 		#Example 1
-		beta=0.0001
+		beta=0.001
 		return ambient_infection*beta
 
 
