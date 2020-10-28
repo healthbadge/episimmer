02e52dd0f3218c532ab331cf48b74c23dfb5a68a
Inavamsi Enaganti <inav65@gmail.com>
2020-10-28 16:36:44 +0530

    Added UserModel.py

--- examples/Example_3/UserModel.py
+++ examples/Example_3/UserModel.py
@@ -15,50 +15,15 @@ def probabilityOfInfection_fn(p_infected_states_list,contact_agent,c_dict,curren
 	else:
 		return 0
 
-	#Example 2
-	if contact_agent.state=='Symptomatic':
-		return math.tanh(float(c_dict['Time Interval'])*float(c_dict['Intensity']))*p_inf_symp
-	elif contact_agent.state=='Asymptomatic':
-		return math.tanh(float(c_dict['Time Interval'])*float(c_dict['Intensity']))*p_inf_asymp
-	else:
-		return 0
 
-def event_contribute_fn(agent,event_info,location,current_time_step):
-		#Example 1
-		if agent.state=='Symptomatic':
-			return 1
-		elif agent.state=='Asymptomatic':
-			return 0.3
-		else:
-			return 0
-
-		#Example 2
-		susceptibility=1
-		if agent.info['HLA Type']=='A':
-			susceptibility=0.9
-
-		if agent.state=='Symptomatic':
-			return math.tanh(float(event_info['Time Interval']))*(1-location.info['Ventilation'])*susceptibility
-		elif agent.state=='Asymptomatic':
-			return 0.3*math.tanh(float(event_info['Time Interval']))*(1-location.info['Ventilation'])*susceptibility
-
-def event_recieve_fn(agent,ambient_infection,event_info,location,current_time_step):
-	p=math.tanh(ambient_infection*0.3)
-	return p
-
-
-
-
-class UserModel(Model.StochasticModel):
+class UserModel(Model.ScheduledModel):
 	def __init__(self):
-		individual_types=['Susceptible','Exposed','Asymptomatic','Symptomatic','Recovered']
-		infected_states=['Asymptomatic','Symptomatic']
-		Model.StochasticModel.__init__(self,individual_types,infected_states)
-		self.set_transition('Susceptible', 'Exposed', self.p_infection([0.3,0.1],probabilityOfInfection_fn))
-		self.set_transition('Exposed', 'Symptomatic', self.p_standard(0.15))
-		self.set_transition('Exposed', 'Asymptomatic', self.p_standard(0.2))
-		self.set_transition('Symptomatic', 'Recovered', self.p_standard(0.2))
-		self.set_transition('Asymptomatic', 'Recovered', self.p_standard(0.2))
+		Model.ScheduledModel.__init__(self)
+		self.insert_state('Susceptible',None, None,self.p_infection([0.3,0.1],probabilityOfInfection_fn,{'Exposed':1}),False)
+		self.insert_state('Exposed',5,2,self.scheduled({'Symptomatic':0.3,'Asymptomatic':0.7}),False)
+		self.insert_state('Symptomatic',11,5,self.scheduled({'Recovered':1}),True)
+		self.insert_state('Asymptomatic',6,3,self.scheduled({'Recovered':1}),True)
+		self.insert_state('Recovered',100, 0,self.scheduled({'Recovered':1}),False)
 
-		self.set_event_contribution_fn(event_contribute_fn)
-		self.set_event_recieve_fn(event_recieve_fn)
+		self.set_event_contribution_fn(None)
+		self.set_event_recieve_fn(None)
