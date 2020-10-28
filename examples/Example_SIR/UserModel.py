6b82961d25b7a8afe9e03aa5279cca8f6a378bca
Inavamsi Enaganti <inav65@gmail.com>
2020-10-28 16:34:40 +0530

    Added UserModel.py

---
+++ examples/Example_SIR/UserModel.py
@@ -0,0 +1,26 @@
+import Model
+
+def event_contribute_fn(agent,event_info,location,current_time_step):
+		if agent.state=='Infected':
+			return 1
+		return 0
+
+def event_recieve_fn(agent,ambient_infection,event_info,location,current_time_step):
+	#Example 1
+	beta=0.001
+	return ambient_infection*beta
+
+
+class UserModel(Model.StochasticModel):
+	def __init__(self):
+		individual_types=['Susceptible','Infected','Recovered','ICU','Dead']
+		infected_states=['Infected']
+		Model.StochasticModel.__init__(self,individual_types,infected_states)
+		self.set_transition('Susceptible', 'Infected', self.p_infection(None,None))
+		self.set_transition('Infected', 'Recovered', self.p_standard(0.2))
+		self.set_transition('Infected', 'ICU', self.p_standard(0.1))
+		self.set_transition('ICU', 'Recovered', self.p_standard(0.2))
+		self.set_transition('ICU', 'Dead', self.p_standard(0.05))
+
+		self.set_event_contribution_fn(event_contribute_fn)
+		self.set_event_recieve_fn(event_recieve_fn)
