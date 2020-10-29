2d55b6ccd246b52f0d6f643bfd510e2973cb521c
Inavamsi Enaganti <inav65@gmail.com>
2020-10-29 14:57:16 +0530

    example changed to reflect changes in episimmer

--- examples/Example_SIR/UserModel.py
+++ examples/Example_SIR/UserModel.py
@@ -15,12 +15,15 @@ class UserModel(Model.StochasticModel):
 	def __init__(self):
 		individual_types=['Susceptible','Infected','Recovered','ICU','Dead']
 		infected_states=['Infected']
-		Model.StochasticModel.__init__(self,individual_types,infected_states)
+		state_proportion={
+							'Susceptible':0.99,
+							'Infected':0.01,
+							'Recovered':0
+						}
+		Model.StochasticModel.__init__(self,individual_types,infected_states,state_proportion)
 		self.set_transition('Susceptible', 'Infected', self.p_infection(None,None))
 		self.set_transition('Infected', 'Recovered', self.p_standard(0.2))
-		self.set_transition('Infected', 'ICU', self.p_standard(0.1))
-		self.set_transition('ICU', 'Recovered', self.p_standard(0.2))
-		self.set_transition('ICU', 'Dead', self.p_standard(0.05))
+
 
 		self.set_event_contribution_fn(event_contribute_fn)
 		self.set_event_recieve_fn(event_recieve_fn)
