9f924dd8866bd97c57c4484655a7bdc990f82396
Inavamsi Enaganti <inav65@gmail.com>
2020-11-05 14:02:18 +0530

    Added restrictions

--- examples/Example_1/Generate_policy.py
+++ examples/Example_1/Generate_policy.py
@@ -4,11 +4,15 @@ def generate_policy():
 	policy_list=[]
 
 	def lockdown_fn(time_step):
-
+		#if time_step%10<2:
+		#	return True
 		return False
 
 	policy_list.append(Policy.full_lockdown(lockdown_fn))
 
-	return policy_list
+	def event_restriction_fn(agent,event_info,current_time_step):
+		return False
+
+	return policy_list,event_restriction_fn
 
 
