808e51329a3652245e02dd097f97445b139157f0
Inavamsi Enaganti <inav65@gmail.com>
2020-12-05 15:17:25 +0530

    Add files via upload

---
+++ examples/Complex Probablistic Lockdown Model/Generate_policy.py
@@ -0,0 +1,18 @@
+import Policy
+
+def generate_policy():
+	policy_list=[]
+
+	def lockdown_fn(time_step):
+		#if time_step%10<2:
+		#	return True
+		return False
+
+	policy_list.append(Policy.full_lockdown(lockdown_fn))
+
+	def event_restriction_fn(agent,event_info,current_time_step):
+		return False
+
+	return policy_list,event_restriction_fn
+
+
