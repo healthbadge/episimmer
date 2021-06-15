e78215d2a30d1af5dbec875ae0dc6f84338fa735
suryadheeshjith <Surya.dheeshjith@gmail.com>
2021-06-15 16:47:16 +0530

    Added External prevalence module

--- src/Model.py
+++ src/Model.py
@@ -9,7 +9,7 @@ class StochasticModel():
 		self.infected_states=infected_states
 		self.state_proportion=state_proportion
 		self.name='Stochastic Model'
-
+		self.external_prev_fn = lambda x,y:0.0
 		self.reset()
 
 	def reset(self):
@@ -74,7 +74,8 @@ class StochasticModel():
 
 			for p in agent.event_probabilities:
 				p_not_inf*=(1-p)
-			return 1 - p_not_inf
+
+			return (1 - p_not_inf) +  self.external_prev_fn(agent, current_time_step)
 
 	def p_infection(self,p_infected_states_list,fn):
 		return partial(self.full_p_infection,fn,p_infected_states_list)
@@ -87,6 +88,9 @@ class StochasticModel():
 
 	def set_event_recieve_fn(self,fn):
 		self.recieve_fn=fn
+
+	def set_external_prevalence_fn(self,fn):
+		self.external_prev_fn = fn
 
 	def update_event_infection(self,event_info,location,agents_obj,current_time_step,event_restriction_fn):
 		ambient_infection=0
