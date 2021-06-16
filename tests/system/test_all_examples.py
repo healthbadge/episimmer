778a5aa946831811a78f644bf2008428af68e19b
suryadheeshjith <Surya.dheeshjith@gmail.com>
2021-06-17 02:59:21 +0530

    Minor fixes

--- tests/system/test_all_examples.py
+++ tests/system/test_all_examples.py
@@ -1,16 +1,16 @@
 import unittest
 import subprocess
 import os
+import os.path as osp
 
 class TestExamples(unittest.TestCase):
     def test_examples(self):
-        examples_list = [p for p in os.listdir("examples") if os.path.isdir(os.path.join("examples",p))]
+        examples_list = [p for p in os.listdir("examples") if osp.isdir(osp.join("examples",p))]
 
         for example in examples_list:
-            main_path = os.path.join("src","Main.py")
-            example_path = os.path.join("examples",example)
-            # error = os.system("python3 {0} {1} -noplot".format(main_path,example_path))
-            process = subprocess.run("python3 {0} {1} -noplot".format(main_path,example_path),shell=True)
+            main_path = osp.join("src","Main.py")
+            example_path = osp.join("examples",example)
+            process = subprocess.run("python3 {0} {1} -noplot".format(main_path,example_path),shell=True,stdout=subprocess.DEVNULL)
             if(process.returncode):
                 raise Exception('Example {0} could not be run!'.format(example))
 
