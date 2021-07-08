42375677072cc7ae7e1a76180d0aee2c2716070b
Apar Ahuja <aparahuja@Apars-MacBook-Air.local>
2021-07-08 19:41:02 +0530

    fix 3D_Automaton

--- examples/3D_Cellular_Automaton/generate_files.py
+++ examples/3D_Cellular_Automaton/generate_files.py
@@ -1,10 +1,4 @@
-from csv import DictWriter
-import random
-import copy
-import numpy as np
 import sys
-from math import sqrt
-
 
 def write_agents(filename,n):
 	header='Agent Index'
@@ -20,42 +14,39 @@ def write_interactions(filename, n):
 	header = 'Agent Index:Interacting Agent Index'
 
 	f = open(filename, 'w')
-	f.write(str(4*n*(n - 1) + n*n)+'\n')
+	f.write(str(8*n*(n - 1) + 2*n*n)+'\n')
 	f.write(header+'\n')
 
 	for i in range(1, n*n + 1):
 		f.write(str(2*i) + ':' + str(2*i - 1) + '\n')
 		f.write(str(2*i - 1) + ':' + str(2*i) + '\n')
 
-		if i%3 != 1:
+		if i%n != 1:
 			f.write(str(2*i) + ':' + str(2*i - 2) + '\n')
-			f.write(str(2*i-1) + ':' + str(2*i - 3) + '\n')
-			f.write(str(2*i - 2) + ':' + str(2*i) + '\n')
-			f.write(str(2*i-3) + ':' + str(2*i - 1) + '\n')
+			f.write(str(2*i - 1) + ':' + str(2*i - 3) + '\n')
+			#f.write(str(2*i - 2) + ':' + str(2*i) + '\n')
+			#f.write(str(2*i - 3) + ':' + str(2*i - 1) + '\n')
 
-		if i%3 != 0:
+		if i%n != 0:
 			f.write(str(2*i) + ':' + str(2*i + 2) + '\n')
 			f.write(str(2*i - 1) + ':' + str(2*i + 1) + '\n')
-			f.write(str(2*i + 2) + ':' + str(2*i) + '\n')
-			f.write(str(2*i + 1) + ':' + str(2*i - 1) + '\n')
+			#f.write(str(2*i + 2) + ':' + str(2*i) + '\n')
+			#f.write(str(2*i + 1) + ':' + str(2*i - 1) + '\n')
 
 		if 1 <= 2*i - 2*n <= 2*n*n:
 			f.write(str(2*i) + ':' + str(2*i - 2*n) + '\n')
-			f.write(str(2*i - 2*n) + ':' + str(2*i) + '\n')
+			#f.write(str(2*i - 2*n) + ':' + str(2*i) + '\n')
 		if 1 <= 2*i + 2*n <= 2*n*n:
 			f.write(str(2*i) + ':' + str(2*i + 2*n) + '\n')
-			f.write(str(2*i + 2*n) + ':' + str(2*i) + '\n')
+			#f.write(str(2*i + 2*n) + ':' + str(2*i) + '\n')
 		if 1 <= 2*i - 1 - 2*n <= 2*n*n:
 			f.write(str(2*i-1) + ':' + str(2*i - 1 - 2*n) + '\n')
-			f.write(str(2*i - 1 - 2*n) + ':' + str(2*i - 1) + '\n')
+			#f.write(str(2*i - 1 - 2*n) + ':' + str(2*i - 1) + '\n')
 		if 1 <= 2*i - 1 + 2*n <= 2*n*n:
 			f.write(str(2*i - 1) + ':' + str(2*i - 1 + 2*n) + '\n')
-			f.write(str(2*i - 1 + 2*n) + ':' + str(2*i - 1) + '\n')
+			#f.write(str(2*i - 1 + 2*n) + ':' + str(2*i - 1) + '\n')
 
 	f.close()
-
-
-
 
 n = int(sys.argv[1])
 no_of_agents = 2*n*n
