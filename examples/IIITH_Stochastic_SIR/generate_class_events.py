0a47eb23f3f4c2adcf1abcd133d7b6242fee694a
inavamsi <inav65.com>
2021-08-06 17:51:41 +0530

    renaming examples

---
+++ examples/IIITH_Stochastic_SIR/generate_class_events.py
@@ -0,0 +1,4 @@
+filename1 = 'Data/classes.csv'
+filename2 = 'Data/timetable.csv'
+
+event_files=['Monday.txt','Tuesday.txt','Wednesday.txt','Thursday.txt','Friday.txt','Saturday.txt','Sunday.txt']
---
+++ examples/IIITH_Stochastic_SIR/generate_fixed_events.py
@@ -0,0 +1,1 @@
+<hostel_mess.txt>
