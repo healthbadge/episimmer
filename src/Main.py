34c39542ac36c54158e7dbfe4e0da1c1c7941cef
suryadheeshjith <Surya.dheeshjith@gmail.com>
2021-08-19 20:42:14 +0530

    Added base vd module. Still need to connect vul_config to readfile and then to VD.py

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@ import ReadFile
 import World
 import Utility
 import Statistics
+from vulnerability_detection.VD import VD
 import os
 import os.path as osp
 
@@ -15,8 +16,8 @@ def get_policy(example_path):
     policy_list, event_restriction_fn = Generate_policy.generate_policy()
     return policy_list, event_restriction_fn
 
-def get_config_path(path):
-    config_filepath = osp.join(path, 'config.txt')
+def get_config_path(path, filename):
+    config_filepath = osp.join(path, filename)
     return config_filepath
 
 @Statistics.write_stats("stats.pickle", "stats.txt")
@@ -24,7 +25,8 @@ def main():
     args = Utility.parse_args()
 
     example_path = args.example_path
-    config_filename = get_config_path(example_path)
+    vuldetect = args.vuldetect
+    config_filename = get_config_path(example_path, 'config.txt')
 
     # Read Config file using ReadFile.ReadConfiguration
     config_obj=ReadFile.ReadConfiguration(config_filename)
@@ -43,8 +45,20 @@ def main():
         os.mkdir(osp.join(config_obj.example_path,'results'))
 
     # Creation of World object
-    world_obj = World.World(config_obj, model, policy_list, event_restriction_fn, agents_filename, interactions_files_list,probabilistic_interactions_files_list, locations_filename, events_files_list, one_time_event_file)
-    world_obj.simulate_worlds()
+    world_obj = World.World(config_obj, model, policy_list, event_restriction_fn, agents_filename, interactions_files_list, probabilistic_interactions_files_list, locations_filename, events_files_list, one_time_event_file)
+
+    if vuldetect:
+        # vd_config_filename = get_config_path(example_path, 'vd_config.txt')
+        # vd_config_obj=ReadFile.Read_VD_Configuration(vd_config_filename)
+        # VD_obj = VD(vd_config_obj, world_obj)
+        # VD_obj.run_vul_detection()
+
+        # Testing
+        VD_obj = VD(None, world_obj)
+        VD_obj.test_run_vul_detection()
+
+    else:
+        world_obj.simulate_worlds()
 
 if __name__=="__main__":
     main()
