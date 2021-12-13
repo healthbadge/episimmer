915ca7f58d715844b75035e6df07b5cc68a1240d
suryadheeshjith <Surya.dheeshjith@gmail.com>
2021-12-13 22:39:24 +0530

    Fixed 3D cellular automata

--- examples/3D_Cellular_Automaton/generate_files.py
+++ examples/3D_Cellular_Automaton/generate_files.py
@@ -10,37 +10,34 @@ def write_agents(filename,n):
 	for i in range(1, n + 1):
 		f.write(str(i-1)+'\n')
 
-def write_interactions(filename, n):
+def write_interactions(filename, n, k):
 	header = 'Agent Index:Interacting Agent Index'
 
 	f = open(filename, 'w')
-	f.write(str(8*n*(n - 1) + 2*n*n)+'\n')
+	# k*n*(n - 1)
+	f.write(str(2*(k*2*n*(n - 1) + n*n*(k-1)))+'\n')
 	f.write(header+'\n')
 
-	for i in range(1, n*n + 1):
-		f.write(str(2*i-1) + ':' + str(2*i - 2) + '\n')
-		f.write(str(2*i - 2) + ':' + str(2*i-1) + '\n')
+	for l in range(k):
+		for i in range(n*n):
+			if l != k-1:
+				f.write(str(i+l*n*n) + ':' + str(i +(l+1)*n*n) + '\n')
+				f.write(str(i+(l+1)*n*n) + ':' + str(i +l*n*n) + '\n')
 
-		if i%n != 1:
-			f.write(str(2*i-1) + ':' + str(2*i - 3) + '\n')
-			f.write(str(2*i - 2) + ':' + str(2*i - 4) + '\n')
+			if i<n*n-n:
+				f.write(str(i+l*n*n) + ':' + str(i+l*n*n+n) + '\n')
+				f.write(str(i+l*n*n+n) + ':' + str(i+l*n*n) + '\n')
 
-		if i%n != 0:
-			f.write(str(2*i-1) + ':' + str(2*i + 1) + '\n')
-			f.write(str(2*i - 2) + ':' + str(2*i) + '\n')
-
-		if 1 <= 2*i - 2*n <= 2*n*n:
-			f.write(str(2*i-1) + ':' + str(2*i - 2*n-1) + '\n')
-		if 1 <= 2*i + 2*n <= 2*n*n:
-			f.write(str(2*i-1) + ':' + str(2*i + 2*n-1) + '\n')
-		if 1 <= 2*i - 1 - 2*n <= 2*n*n:
-			f.write(str(2*i-2) + ':' + str(2*i - 2 - 2*n) + '\n')
-		if 1 <= 2*i - 1 + 2*n <= 2*n*n:
-			f.write(str(2*i - 2) + ':' + str(2*i - 2 + 2*n) + '\n')
+			if (i+1) % n != 0:
+				f.write(str(i+l*n*n) + ':' + str(i+l*n*n+1) + '\n')
+				f.write(str(i+l*n*n+1) + ':' + str(i+l*n*n) + '\n')
 
 	f.close()
 
+# n x n x k agents  
 n = int(sys.argv[1])
-no_of_agents = 2*n*n
+k = int(sys.argv[2])
+
+no_of_agents = k*n*n
 write_agents('agents.txt', no_of_agents)
-write_interactions('interactions.txt', n)
+write_interactions('interactions.txt', n, k)
