f8eeb16d3b74e5e8e62b576bada42784b8494ebb
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-01-08 23:20:16 +0530

    setup.py fix

--- setup.py
+++ setup.py
@@ -8,8 +8,31 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-setup(name='episimmer', version='0.1.0',
-      description='Epidemic Simulation Platform', long_description=readme,
-      author='HealthBadge', author_email='episimmer@healthbadge.org',
-      url='https://github.com/healthbadge/episimmer', license=license,
-      packages=find_packages(exclude=('tests', 'docs', 'examples')))
+
+install_requires = [
+    'matplotlib==3.4.2'
+    'numpy==1.17.2'
+    'pyvis==0.1.9'
+]
+tests_require = ['pytest', 'pytest-cov']
+dev_requires = tests_require + ['pre-commit']
+
+
+setup(
+    name='episimmer',
+    version='0.1.0',
+    description='Epidemic Simulation Platform',
+    long_description=readme,
+    author='HealthBadge',
+    author_email='episimmer@healthbadge.org',
+    url='https://github.com/healthbadge/episimmer',
+    license=license,
+    python_requires='>=3.7',
+    install_requires=install_requires,
+    extras_require={
+        'test': tests_require,
+        'dev': dev_requires,
+    },
+    packages=find_packages(exclude=('tests', 'docs', 'examples')),
+    include_package_data=True
+    )
