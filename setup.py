f8273b13282b6c31af22eeead6c5c61295437eac
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-01-09 04:50:43 +0530

    Fixed setup.py and easy requirements read

--- setup.py
+++ setup.py
@@ -1,38 +1,43 @@
-#!/usr/bin/env python3
-
 from setuptools import find_packages, setup
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
-
-
-install_requires = [
-    'matplotlib==3.4.2',
-    'numpy==1.17.2',
-    'pyvis==0.1.9'
+NAME = 'episimmer'
+VERSION = '1.0.0'
+DESCRIPTION = 'Epidemic Simulation Platform'
+URL = 'https://github.com/healthbadge/episimmer'
+EMAIL = 'inavamsi@healthbadge.org, surya@healthbadge.org'
+AUTHOR = 'Inavamsi Enaganti, Surya Dheeshjith'
+LICENSE = 'BSD-3-Clause'
+PYTHON = '>=3.7'
+CLASSIFIERS = [
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Topic :: Scientific/Engineering',
 ]
-tests_require = ['pytest', 'pytest-cov']
-dev_requires = tests_require + ['pre-commit']
 
+with open('README.md') as f:
+    LONG_DESC = f.read()
+
+with open('requirements.txt') as f:
+    REQUIRES = f.read()
+
+with open('docs/requirements-dev.txt') as f:
+    DEV_REQUIRES = f.read()
 
 setup(
-    name='episimmer',
-    version='0.1.0',
-    description='Epidemic Simulation Platform',
-    long_description=readme,
-    author='HealthBadge',
-    author_email='episimmer@healthbadge.org',
-    url='https://github.com/healthbadge/episimmer',
-    license=license,
-    python_requires='>=3.8',
-    install_requires=install_requires,
-    extras_require={
-        'test': tests_require,
-        'dev': dev_requires,
-        },
-    packages=find_packages(exclude=('tests', 'docs', 'examples')),
-    include_package_data=True
-    )
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESC,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    install_requires=REQUIRES,
+    extras_require={'test': DEV_REQUIRES},
+    packages=find_packages(exclude=['tests', 'docs', 'examples'])
+)
