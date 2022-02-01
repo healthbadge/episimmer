717c8b75a8f28b30f3f089e52dbd9e80a6ce2333
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-02-01 20:27:35 +0530

    Documentation - location.py

--- episimmer/location.py
+++ episimmer/location.py
@@ -1,14 +1,29 @@
+from typing import Dict, List, Union
+
+
 class Location():
-    def __init__(self, info_dict):
-        self.info = info_dict
-        self.index = info_dict['Location Index']
-        self.events = []
-        self.lock_down_state = False
+    """
+    Class for a location.
 
-    def new_time_step(self):
+    Args:
+        info_dict: Information of each location taken from the locations file .
+    """
+    def __init__(self, info_dict: Dict[str, str]):
+        self.info: Dict[str, str] = info_dict
+        self.index: str = info_dict['Location Index']
+        self.events: List[Dict[str, Union[str, List[str]]]] = []
+        self.lock_down_state: bool = False
+
+    def new_time_step(self) -> None:
+        """
+        Resets the lockdown state and events of the location at the start of each timestep
+        """
         self.lock_down_state = False
         self.events = []
 
-    def add_event(self, event_info):
+    def add_event(self, event_info: Dict[str, List[str]]) -> None:
+        """
+        Storing an event that take place in the location
+        """
         if not self.lock_down_state:
             self.events.append(event_info)
