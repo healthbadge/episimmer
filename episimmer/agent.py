2ac9eb2c37e1420927a0790cce267ea1cc72ca24
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-02-02 02:19:44 +0530

    Minor fixes - agent.py

--- episimmer/agent.py
+++ episimmer/agent.py
@@ -3,11 +3,11 @@ from typing import Dict, List, Tuple, Union
 
 class Agent():
     """
-        Class for an agent of the simulation.
+    Class for an agent of the simulation.
 
-        Args:
-        state: The state of the agent.
-        info_dict: Information of each agent taken from the agents file.
+    Args:
+    state: The state of the agent.
+    info_dict: Information of each agent taken from the agents file.
     """
     def __init__(self, state: str, info_dict: Dict[str, str]):
         self.state: str = state
