01b8d0154c9b9506bce7af2704ef4b98768d562d
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-02-05 04:13:36 +0530

    utils py documentation
    Co-authored-by: Anurita Bose <anuritabs@gmail.com>
    Co-authored-by: Tushar Shetty <tusharshetty61@gmail.com>

--- episimmer/utils/time.py
+++ episimmer/utils/time.py
@@ -1,22 +1,46 @@
+from typing import Union
+
+
 class Time():
-    current_world = None
-    current_time_step = None
+    """
+    Class that handles the time step and world of simulation.
+    """
+    current_world: Union[int, None] = None
+    current_time_step: Union[int, None] = None
 
     @staticmethod
-    def get_current_world():
+    def get_current_world() -> int:
+        """
+        Returns the current world of simulation.
+
+        Returns:
+            Index of current world
+        """
         return Time.current_world
 
     @staticmethod
-    def get_current_time_step():
+    def get_current_time_step() -> int:
+        """
+        Returns the current time step of simulation.
+
+        Returns:
+            Index of current time step
+        """
         return Time.current_time_step
 
     @staticmethod
-    def reset():
+    def reset() -> None:
+        """
+        Resets the world and time step at the beginning of a new simulation.
+        """
         Time.current_world = None
         Time.current_time_step = None
 
     @staticmethod
-    def new_world():
+    def new_world() -> None:
+        """
+        Sets the value of current_world and current time step at the onset of a world.
+        """
         if Time.current_world is None:
             Time.current_world = 0
         else:
@@ -25,5 +49,8 @@ class Time():
         Time.current_time_step = 0
 
     @staticmethod
-    def increment_current_time_step():
+    def increment_current_time_step() -> None:
+        """
+        increments the current time step during simulation.
+        """
         Time.current_time_step += 1
