3e202f0a904184fa3ad7ad7da94b96035f8fc4d4
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-02-05 22:01:20 +0530

    Documentation - statistics.py
    Co-authored-by: Anurita Bose <anuritabs@gmail.com>
    Co-authored-by: Tushar Shetty <tusharshetty61@gmail.com>

--- episimmer/utils/statistics.py
+++ episimmer/utils/statistics.py
@@ -3,80 +3,94 @@ import functools
 import os
 import pickle
 import pprint
+from typing import TYPE_CHECKING, Callable, Dict, List, Tuple, Union
 
 from .arg_parser import parse_args
 from .time import Time
 
-
-def is_obj(obj):
-    return hasattr(obj, '__dict__')
-
-
-def is_iter(obj):
-    return hasattr(obj, '__iter__')
-
-
-def is_list(obj):
-    return isinstance(obj, list)
-
-
-def is_dict(obj):
-    return isinstance(obj, dict)
-
-
-def expand_obj(obj):
-    return vars(obj)
+if TYPE_CHECKING:
+    from episimmer.simulate import Simulate
 
 
 class Stats():
-    d = {}
+    """
+    Class to handle variable statistics in Episimmer
+    """
+    stats_dict = {}
 
     @staticmethod
-    def add_content(key, content):
+    def add_content(key: str, content: Dict[str, Dict]) -> None:
+        """
+        Adds content to the statistics dictionary into a list.
+
+        Args:
+            key: Key to be added to dictionary
+            content: Content to be added as value to the key in dictionary
+        """
         world = Time.get_current_world()
         time_step = Time.get_current_time_step()
-        if world not in Stats.d.keys():
-            Stats.d[world] = {}
+        if world not in Stats.stats_dict.keys():
+            Stats.stats_dict[world] = {}
 
-        if time_step not in Stats.d[world].keys():
-            Stats.d[world][time_step] = {}
+        if time_step not in Stats.stats_dict[world].keys():
+            Stats.stats_dict[world][time_step] = {}
 
-        if key not in Stats.d[world][time_step].keys():
-            Stats.d[world][time_step][key] = []
+        if key not in Stats.stats_dict[world][time_step].keys():
+            Stats.stats_dict[world][time_step][key] = []
 
-        Stats.d[world][time_step][key].append(content)
+        Stats.stats_dict[world][time_step][key].append(content)
 
     @staticmethod
-    def get_dict():
-        return Stats.d
+    def get_dict() -> Dict[str, Dict]:
+        """
+        Returns the statistics dictionary
+
+        Returns:
+            the statistics dictionary
+        """
+        return Stats.stats_dict
 
 
-def expand_levels_recursion(obj, levels, cur_level):
+def expand_levels_recursion(obj: object, levels: int,
+                            cur_level: int) -> Union[Dict[str, Dict], object]:
+    """
+    This function is a recursive function which expands the current object's variables
+    to a given number of levels. If the object is a dictionary or an iterable,only then
+    will the object be expanded. Other objects are returned as is.
+
+    Args:
+        obj: Object to be expanded
+        levels: Number of levels to expand the object
+        cur_level: Current level expanded
+
+    Returns:
+        An expanded object
+    """
 
     if cur_level == levels:
         return obj
 
     stats_dict = {}
-    if is_obj(obj):
-        stats_dict[str(obj)] = expand_levels_recursion(expand_obj(obj), levels,
+    if hasattr(obj, '__dict__'):
+        stats_dict[str(obj)] = expand_levels_recursion(vars(obj), levels,
                                                        cur_level + 1)
-    elif is_iter(obj):
-        if is_list(obj):
+    elif hasattr(obj, '__iter__'):
+        if isinstance(obj, list):
             for obj_i in obj:
-                if is_obj(obj_i):
+                if hasattr(obj_i, '__dict__'):
                     stats_dict[str(obj_i)] = expand_levels_recursion(
-                        expand_obj(obj_i), levels, cur_level + 1)
-                elif is_iter(obj_i):
+                        vars(obj_i), levels, cur_level + 1)
+                elif hasattr(obj_i, '__iter__'):
                     stats_dict[str(obj_i)] = expand_levels_recursion(
                         obj_i, levels, cur_level + 1)
                 else:
                     return obj
-        elif is_dict(obj):
+        elif isinstance(obj, dict):
             for obj_i in obj.keys():
-                if is_obj(obj[obj_i]):
+                if hasattr(obj[obj_i], '__dict__'):
                     stats_dict[str(obj_i)] = expand_levels_recursion(
-                        expand_obj(obj[obj_i]), levels, cur_level + 1)
-                elif is_iter(obj[obj_i]):
+                        vars(obj[obj_i]), levels, cur_level + 1)
+                elif hasattr(obj[obj_i], '__iter__'):
                     stats_dict[str(obj_i)] = expand_levels_recursion(
                         obj[obj_i], levels, cur_level + 1)
                 else:
@@ -87,12 +101,36 @@ def expand_levels_recursion(obj, levels, cur_level):
     return stats_dict
 
 
-def expand_levels(obj, levels):
+def expand_levels(obj: object, levels: int) -> Union[Dict[str, Dict], object]:
+    """
+    Expands the object by expanding the members of the object such as dictionaries and iterables. Calls the
+    expand_levels_recursion method for expansion of object.
+
+    Args:
+        obj: Object to be expanded
+        levels: Number of levels to expand the object
+
+    Returns:
+        An expanded object
+    """
     return expand_levels_recursion(obj, levels, 0)
 
 
-def process_dict_recursion(stats_dict, final_level_properties, levels,
-                           cur_level):
+def process_dict_recursion(stats_dict: Dict[str, Dict],
+                           final_level_properties: Union[str, List[str]],
+                           levels: int, cur_level: int) -> Dict[str, Dict]:
+    """
+    This function is a recursive function which selects the variables of the dictionary to be saved.
+
+    Args:
+        stats_dict: Dictionary containing an object's expanded members
+        final_level_properties: Properties required to be saved
+        levels: NNumber of levels the object was expanded
+        cur_level: Current level of dictionary
+
+    Returns:
+        Processed dictionary
+    """
     if cur_level == levels:
         for key in list(stats_dict):
             if key not in final_level_properties:
@@ -100,7 +138,7 @@ def process_dict_recursion(stats_dict, final_level_properties, levels,
         return stats_dict
 
     for key in list(stats_dict):
-        if is_dict(stats_dict[key]):
+        if isinstance(stats_dict[key], dict):
             ret_dict = process_dict_recursion(stats_dict[key],
                                               final_level_properties, levels,
                                               cur_level + 1)
@@ -112,10 +150,25 @@ def process_dict_recursion(stats_dict, final_level_properties, levels,
     return stats_dict
 
 
-def process_dict(stats_dict, final_level_properties, levels):
+def process_dict(stats_dict: Dict[str, Dict],
+                 final_level_properties: Union[str, List[str]],
+                 levels: int) -> Dict[str, Dict]:
+    """
+    Processes the expanded object as a dictionary by choosing to save only required properties.
+    Calls the process_dict_recursion method.
+
+    Args:
+        stats_dict: Dictionary containing an object's expanded members
+        final_level_properties: Properties required to be saved
+        levels: NNumber of levels the object was expanded
+
+    Returns:
+        Processed dictionary
+
+    """
     if final_level_properties == 'All':
         return stats_dict
-    elif not is_list(final_level_properties):
+    elif not isinstance(final_level_properties, list):
         raise Exception(
             "Final level properties must either be a list or contain value 'All'"
         )
@@ -123,28 +176,70 @@ def process_dict(stats_dict, final_level_properties, levels):
                                   0)
 
 
-def get_pretty_print_str(stats_dict):
+def get_pretty_print_str(stats_dict: Dict[str, Dict]) -> str:
+    """
+    Returns the dictionary as a string in pretty print format
+
+    Args:
+        stats_dict: Dictionary to be converted to pretty print string
+
+    Returns:
+        String representing dictionary in pretty print format
+
+    """
     val_string = pprint.pformat(stats_dict, indent=0)
     val_string += '\n'
     return val_string
 
 
-def save_pickle(example_path, pickle_file, final_dict):
+def save_pickle(example_path: str, pickle_file: str,
+                final_dict: Dict[str, Dict]) -> None:
+    """
+    Saves the dictionary into a pickle file
+
+    Args:
+        example_path: Path to directory containing simulation files.
+        pickle_file: Name of pickle file
+        final_dict: Dictionary to be saved
+    """
     with open(os.path.join(example_path, 'results', pickle_file),
               'wb') as handle:
         pickle.dump(final_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def save_to_text_file(example_path, string, text_filename):
+def save_to_text_file(example_path: str, string: str,
+                      text_filename: str) -> None:
+    """
+    Saves the string to a text file.
+
+    Args:
+        example_path: Path to directory containing simulation files.
+        string: String to be saved
+        text_filename: Name of text file
+    """
     fp = open(os.path.join(example_path, 'results', text_filename), 'w')
     fp.write(string)
     fp.close()
 
 
-def save_stats(obj_lev_tuples, key, final_level_properties='All'):
-    def decorator(func):
+def save_stats(
+        obj_lev_tuples: List[Tuple[str, int]],
+        key: str,
+        final_level_properties: Union[str, List[str]] = 'All') -> Callable:
+    """
+    Decorator to save statistics (object members) into a dictionary
+
+    Args:
+        obj_lev_tuples: List of tuples containing the object to be saved (as a string) and the number of levels
+        key: Key to use while saving the content in the statistics dictionary
+        final_level_properties: Properties to save. Can be a list of properties/members of the object or 'All'
+
+    Returns:
+        Callable function
+    """
+    def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
-        def wrapper(ref, *args, **kwargs):
+        def wrapper(ref: 'Simulate', *args, **kwargs) -> None:
             func(ref, *args, **kwargs)
             args = parse_args()
             stats = args.stats
@@ -164,8 +259,18 @@ def save_stats(obj_lev_tuples, key, final_level_properties='All'):
     return decorator
 
 
-def write_stats(pickle_file, text_file):
-    def decorator(func):
+def write_stats(pickle_file: str, text_file: str) -> Callable:
+    """
+    Decorator to write statistics (object members) into a file
+
+    Args:
+        pickle_file: Name of pickle file
+        text_file: Name of text file
+
+    Returns:
+        Callable function
+    """
+    def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             func(*args, **kwargs)
