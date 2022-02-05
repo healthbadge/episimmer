a68f425df6f00d37dedc44b60425a1311e51b17d
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-02-05 19:58:28 +0530

    Reformatted statistics.py

--- episimmer/utils/statistics.py
+++ episimmer/utils/statistics.py
@@ -7,31 +7,44 @@ import pprint
 from .arg_parser import parse_args
 from .time import Time
 
-is_obj = lambda obj: hasattr(obj, '__dict__')
-is_iter = lambda obj: hasattr(obj, '__iter__')
-is_list = lambda obj: isinstance(obj, list)
-is_dict = lambda obj: isinstance(obj, dict)
 
-expand_obj = lambda obj: vars(obj)
+def is_obj(obj):
+    return hasattr(obj, '__dict__')
+
+
+def is_iter(obj):
+    return hasattr(obj, '__iter__')
+
+
+def is_list(obj):
+    return isinstance(obj, list)
+
+
+def is_dict(obj):
+    return isinstance(obj, dict)
+
+
+def expand_obj(obj):
+    return vars(obj)
 
 
 class Stats():
     d = {}
 
     @staticmethod
-    def add_content(K, C):
-        W = Time.get_current_world()
-        T = Time.get_current_time_step()
-        if (W not in Stats.d.keys()):
-            Stats.d[W] = {}
+    def add_content(key, content):
+        world = Time.get_current_world()
+        time_step = Time.get_current_time_step()
+        if world not in Stats.d.keys():
+            Stats.d[world] = {}
 
-        if (T not in Stats.d[W].keys()):
-            Stats.d[W][T] = {}
+        if time_step not in Stats.d[world].keys():
+            Stats.d[world][time_step] = {}
 
-        if (K not in Stats.d[W][T].keys()):
-            Stats.d[W][T][K] = []
+        if key not in Stats.d[world][time_step].keys():
+            Stats.d[world][time_step][key] = []
 
-        Stats.d[W][T][K].append(C)
+        Stats.d[world][time_step][key].append(content)
 
     @staticmethod
     def get_dict():
@@ -43,73 +56,75 @@ def expand_levels_recursion(obj, levels, cur_level):
     if cur_level == levels:
         return obj
 
-    dict = {}
+    stats_dict = {}
     if is_obj(obj):
-        dict[str(obj)] = expand_levels_recursion(expand_obj(obj), levels,
-                                                 cur_level + 1)
+        stats_dict[str(obj)] = expand_levels_recursion(expand_obj(obj), levels,
+                                                       cur_level + 1)
     elif is_iter(obj):
         if is_list(obj):
             for obj_i in obj:
                 if is_obj(obj_i):
-                    dict[str(obj_i)] = expand_levels_recursion(
+                    stats_dict[str(obj_i)] = expand_levels_recursion(
                         expand_obj(obj_i), levels, cur_level + 1)
                 elif is_iter(obj_i):
-                    dict[str(obj_i)] = expand_levels_recursion(
+                    stats_dict[str(obj_i)] = expand_levels_recursion(
                         obj_i, levels, cur_level + 1)
                 else:
                     return obj
         elif is_dict(obj):
             for obj_i in obj.keys():
                 if is_obj(obj[obj_i]):
-                    dict[str(obj_i)] = expand_levels_recursion(
+                    stats_dict[str(obj_i)] = expand_levels_recursion(
                         expand_obj(obj[obj_i]), levels, cur_level + 1)
                 elif is_iter(obj[obj_i]):
-                    dict[str(obj_i)] = expand_levels_recursion(
+                    stats_dict[str(obj_i)] = expand_levels_recursion(
                         obj[obj_i], levels, cur_level + 1)
                 else:
-                    dict[str(obj_i)] = obj[obj_i]
+                    stats_dict[str(obj_i)] = obj[obj_i]
     else:
         return obj
 
-    return dict
+    return stats_dict
 
 
 def expand_levels(obj, levels):
     return expand_levels_recursion(obj, levels, 0)
 
 
-def process_dict_recursion(dict, final_level_properties, levels, cur_level):
-    if (cur_level == levels):
-        for key in list(dict):
-            if (key not in final_level_properties):
-                del (dict[key])
-        return dict
+def process_dict_recursion(stats_dict, final_level_properties, levels,
+                           cur_level):
+    if cur_level == levels:
+        for key in list(stats_dict):
+            if key not in final_level_properties:
+                del (stats_dict[key])
+        return stats_dict
 
-    for key in list(dict):
-        if (is_dict(dict[key])):
-            ret_dict = process_dict_recursion(dict[key],
+    for key in list(stats_dict):
+        if is_dict(stats_dict[key]):
+            ret_dict = process_dict_recursion(stats_dict[key],
                                               final_level_properties, levels,
                                               cur_level + 1)
-            if (not ret_dict
-                ):  # If dict is empty i.e. {}, delete key in higher dict
-                del (dict[key])
+            # If stats_dict is empty i.e. {}, delete key in higher stats_dict
+            if not ret_dict:
+                del (stats_dict[key])
             else:
-                dict[key] = ret_dict
-    return dict
+                stats_dict[key] = ret_dict
+    return stats_dict
 
 
-def process_dict(dict, final_level_properties, levels):
-    if (final_level_properties == 'All'):
-        return dict
-    elif (not is_list(final_level_properties)):
+def process_dict(stats_dict, final_level_properties, levels):
+    if final_level_properties == 'All':
+        return stats_dict
+    elif not is_list(final_level_properties):
         raise Exception(
             "Final level properties must either be a list or contain value 'All'"
         )
-    return process_dict_recursion(dict, final_level_properties, levels, 0)
+    return process_dict_recursion(stats_dict, final_level_properties, levels,
+                                  0)
 
 
-def get_pretty_print_str(dict):
-    val_string = pprint.pformat(dict, indent=0)
+def get_pretty_print_str(stats_dict):
+    val_string = pprint.pformat(stats_dict, indent=0)
     val_string += '\n'
     return val_string
 
@@ -120,9 +135,9 @@ def save_pickle(example_path, pickle_file, final_dict):
         pickle.dump(final_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def save_to_text_file(example_path, str, text_filename):
+def save_to_text_file(example_path, string, text_filename):
     fp = open(os.path.join(example_path, 'results', text_filename), 'w')
-    fp.write(str)
+    fp.write(string)
     fp.close()
 
 
@@ -133,15 +148,16 @@ def save_stats(obj_lev_tuples, key, final_level_properties='All'):
             func(ref, *args, **kwargs)
             args = parse_args()
             stats = args.stats
-            if (stats):
+            if stats:
                 for obj_str, levels in obj_lev_tuples:
                     obj = getattr(ref, obj_str)
-                    dict = expand_levels(obj, levels)  # Generate nested dict
-                    dict = copy.deepcopy(dict)
-                    dict = process_dict(
-                        dict, final_level_properties,
+                    stats_dict = expand_levels(obj,
+                                               levels)  # Generate nested dict
+                    stats_dict = copy.deepcopy(stats_dict)
+                    stats_dict = process_dict(
+                        stats_dict, final_level_properties,
                         levels)  # Process dict based on desired properties
-                    Stats.add_content(key, dict)
+                    Stats.add_content(key, stats_dict)
 
         return wrapper
 
@@ -155,12 +171,12 @@ def write_stats(pickle_file, text_file):
             func(*args, **kwargs)
             args = parse_args()
             stats = args.stats
-            if (stats):
+            if stats:
                 example_path = args.example_path
                 final_dict = Stats.get_dict()
                 save_pickle(example_path, pickle_file, final_dict)
-                str = get_pretty_print_str(final_dict)
-                save_to_text_file(example_path, str, text_file)
+                stats_str = get_pretty_print_str(final_dict)
+                save_to_text_file(example_path, stats_str, text_file)
 
         return wrapper
 
