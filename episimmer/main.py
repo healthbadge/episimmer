9f7180cece1b2d78564d0dc31872d60b703faa95
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-02-12 19:10:36 +0530

    Refactored and added docstring to main

--- episimmer/main.py
+++ episimmer/main.py
@@ -9,20 +9,21 @@ from episimmer.vulnerability_detection.vd import VD
 from episimmer.world import World
 
 
-@write_stats('stats.pickle', 'stats.txt')
-def main():
-    args = parse_args()
+def get_world_obj(config_obj, example_path):
+    """
+    Returns the world object for the simulation
 
-    example_path = args.example_path
-    vul_detect = args.vuldetect
-    config_filename = osp.join(example_path, 'config.txt')
+    Args:
+        config_obj: Config object populated with configuration from the config.txt file
+        example_path: Path to directory with simulation files
 
-    config_obj = ReadConfiguration(config_filename)
-
-    agents_filename, interactions_files_list_filename,\
-        events_files_list_filename, locations_filename,\
+    Returns:
+        World object
+    """
+    agents_filename, interactions_files_list_filename, \
+        events_files_list_filename, locations_filename, \
         one_time_event_file, probabilistic_interactions_files_list_filename = config_obj.get_file_paths(example_path)
-    interactions_files_list, events_files_list,\
+    interactions_files_list, events_files_list, \
         probabilistic_interactions_files_list = config_obj.get_file_names_list(
                                                         example_path, interactions_files_list_filename,
                                                         events_files_list_filename,
@@ -42,6 +43,21 @@ def main():
                       probabilistic_interactions_files_list,
                       locations_filename, events_files_list,
                       one_time_event_file)
+
+    return world_obj
+
+
+@write_stats('stats.pickle', 'stats.txt')
+def main():
+    """
+    Episimmer run begins here.
+    """
+    args = parse_args()
+
+    example_path = args.example_path
+    vul_detect = args.vuldetect
+    config_obj = ReadConfiguration(osp.join(example_path, 'config.txt'))
+    world_obj = get_world_obj(config_obj, example_path)
 
     if vul_detect:
         vd_config_filename = osp.join(example_path, 'vd_config.txt')
