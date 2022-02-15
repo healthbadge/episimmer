5dd676f8ac6013fa9fe1124588a9aa3b0e3b7cd9
suryadheeshjith <Surya.dheeshjith@gmail.com>
2022-02-16 01:32:23 +0530

    Fixed examples comments

--- examples/Interaction_Spaces/Stochastic_SIR_two_locations/UserModel.py
+++ examples/Interaction_Spaces/Stochastic_SIR_two_locations/UserModel.py
@@ -1,15 +1,14 @@
 import episimmer.model as model
 
-#The two functions event_contribute_fn and event_receive_fn together control the spread of infection
 
-# This function states the amount an agent contributes to ambient infection in the region
-#note that only infected agents contibute to the ambient infection
 def event_contribute_fn(agent,event_info,location,current_time_step):
+
+
 		if agent.state=='Infected':
 			return 1
 		return 0
 
-#This function states the probability of an agent becoming infected from the ambient infection
+
 def event_receive_fn(agent,ambient_infection,event_info,location,current_time_step):
 	beta=0.001
 	return ambient_infection*beta
@@ -17,19 +16,19 @@ def event_receive_fn(agent,ambient_infection,event_info,location,current_time_st
 
 class UserModel(model.StochasticModel):
 	def __init__(self):
-		individual_types=['Susceptible','Infected','Recovered']	#These are the states that will be used by the compartmental model
-		infected_states=['Infected']	#These are the states that can infect
-		state_proportion={				#This is the starting proportions of each state
+		individual_types=['Susceptible','Infected','Recovered']
+		infected_states=['Infected']
+		state_proportion={
 							'Susceptible':0.99,
 							'Infected':0.01,
 							'Recovered':0
 						}
-		model.StochasticModel.__init__(self,individual_types,infected_states,state_proportion)  #We use the inbuilt model in the package
-		self.set_transition('Susceptible', 'Infected', self.p_infection())	#Adding S-> I transition which is redundant in this case as we use the event_contribute and event_receive function
-		self.set_transition('Infected', 'Recovered', self.p_standard(0.2))	#Adding the I->R transition
+		model.StochasticModel.__init__(self,individual_types,infected_states,state_proportion)
+		self.set_transition('Susceptible', 'Infected', self.p_infection())
+		self.set_transition('Infected', 'Recovered', self.p_standard(0.2))
 
 
-		self.set_event_contribution_fn(event_contribute_fn)	#Setting the above defined function into the model
-		self.set_event_receive_fn(event_receive_fn)	#Setting the above defined function into the model
+		self.set_event_contribution_fn(event_contribute_fn)
+		self.set_event_receive_fn(event_receive_fn)
 
 		self.name='Stochastic SIR'
